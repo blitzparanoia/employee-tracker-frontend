588bd079acf2ff8fd6e67ae391d1426ff7395589
blitzparanoia <bluemilk44@gmail.com>
2020-03-17 19:19:46 -0400

    outlined App.js, added the store, applyMiddleware, redux devtools, passed store to the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App';
 
+//connect app to dev tools, with use compose to combine middleware
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+//data is in store for 'global app use', reducer will take the action and return the new updated store
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+//store needs to be passed to the provider to give app access to store
+ReactDOM.render(
+    < Provider store={store} >
+        <App />
+    </ Provider>
+,document.getElementById('root'));
 
