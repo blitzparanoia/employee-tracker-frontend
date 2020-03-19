e28a3e2c17b5b621edfb754aad930b45556f0da6
blitzparanoia <bluemilk44@gmail.com>
2020-03-19 17:12:39 -0400

    passed the data in the action

--- src/actions/addCompany.js
+++ src/actions/addCompany.js
@@ -1,8 +1,8 @@
-export function addCompany() {
+export function addCompany(data) {
     return (dispatch) => {
         fetch('http://localhost:3000/api/v1/companies', {
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
             method: 'POST',
@@ -10,7 +10,7 @@ export function addCompany() {
         })
         .then(response => response.json())
         .then(company => dispatch({
-            type: 'ADD_COMPANY'
+            type: 'ADD_COMPANY',
             payload: 'company'
         }))
     }
