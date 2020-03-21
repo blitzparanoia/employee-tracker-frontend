e7b69edf7e1e4b3af103ff687d83ee82e309efa9
blitzparanoia <bluemilk44@gmail.com>
2020-03-21 11:50:21 -0400

    outline fields for form

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 class EmployeeForm extends React.Component {
+
+  state = {
+    first_name: '',
+    last_name: '',
+    department: '',
+    position: '',
+    active: true
+  }
   render() {
     return(
       <div>
       EmployeeForm
       <form>
-      <input type='text'/>
+        <label>First Name:</label>
+        <input type='text' name='first_name' placeholder='First Name' value={this.state.first_name}/>
+
+        <label>Last Name:</label>
+        <input type='text' name='last_name' placeholder='Last Name' value={this.state.last_name}/>
+
+        <label>Department:</label>
+        <input type='text' name='department' placeholder='Department' value={this.state.value} />
+
+        <label>Position:</label>
+        <input type='text' name='position' placeholder='Position' value={this.state.position}/>
+
+
+
+
+
+
       </form>
       </div>
     )
